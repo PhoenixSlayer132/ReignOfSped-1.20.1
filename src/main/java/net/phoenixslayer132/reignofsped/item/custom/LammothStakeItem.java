777bbaaba1e73bd5aef3e09df36f00f7e721527a
PhoenixSlayer132 <phoenixslayer132@gmail.com>
2024-02-28 20:43:52 -0500

    Added another use for Lammoth Stake * Lammoth Stake    - Now able to check Farmland Moisture level

--- src/main/java/net/phoenixslayer132/reignofsped/item/custom/LammothStakeItem.java
+++ src/main/java/net/phoenixslayer132/reignofsped/item/custom/LammothStakeItem.java
@@ -1,11 +1,18 @@
 package net.phoenixslayer132.reignofsped.item.custom;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.state.property.Properties;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+
+import static net.minecraft.block.FarmlandBlock.MOISTURE;
 
 public class LammothStakeItem extends Item{
     public LammothStakeItem(Settings settings) {
@@ -15,19 +22,31 @@ public class LammothStakeItem extends Item{
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
         if(!context.getWorld().isClient){
+            BlockPos positionClicked = context.getBlockPos();
+
             BlockPos abovePositionClicked = context.getBlockPos().add(0, 1,0);
+            BlockState blockState = context.getWorld().getBlockState(positionClicked);
+            Block block = blockState.getBlock();
 
-
-            if(context.getWorld().hasRain(abovePositionClicked)){
+            if(blockState.getBlock() == Blocks.FARMLAND){
+                int i = blockState.get(MOISTURE);
                 PlayerEntity player = context.getPlayer();
-                player.sendMessage(Text.literal("This block has rain above it."));
-                context.getStack().damage(1, context.getPlayer(), playerEntity -> playerEntity.sendToolBreakStatus(playerEntity.getActiveHand()));
-
-            } else if (!context.getWorld().hasRain(abovePositionClicked)) {
-                PlayerEntity player = context.getPlayer();
-                player.sendMessage(Text.literal("This block does not have rain above it."));
+                player.sendMessage(Text.literal("This farmland's moisture is at " + i + "."));
                 context.getStack().damage(1, context.getPlayer(), playerEntity -> playerEntity.sendToolBreakStatus(playerEntity.getActiveHand()));
             }
+            else{
+                if(context.getWorld().hasRain(abovePositionClicked)){
+                    PlayerEntity player = context.getPlayer();
+                    player.sendMessage(Text.literal("This block has rain above it."));
+                    context.getStack().damage(1, context.getPlayer(), playerEntity -> playerEntity.sendToolBreakStatus(playerEntity.getActiveHand()));
+
+                } else if (!context.getWorld().hasRain(abovePositionClicked)) {
+                    PlayerEntity player = context.getPlayer();
+                    player.sendMessage(Text.literal("This block does not have rain above it."));
+                    context.getStack().damage(1, context.getPlayer(), playerEntity -> playerEntity.sendToolBreakStatus(playerEntity.getActiveHand()));
+                }
+            }
+
         }
 
         return super.useOnBlock(context);
